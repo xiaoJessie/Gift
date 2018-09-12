4dd69bb551d244fa47e8f188685c15c1d5a0bb5a
Jessie <jessie@Jessie-de.local>
2018-09-12 16:00:19 +0800

    简单使用百度Echarts4图表

--- my-project/src/pages/Dashboard/NewPage.js
+++ my-project/src/pages/Dashboard/NewPage.js
@@ -3,6 +3,14 @@ import { connect } from 'dva';
 import { formatMessage, FormattedMessage } from 'umi/locale';
 import { Card, Switch } from 'antd';
 
+// 引入 ECharts 主模块
+import echarts from 'echarts/lib/echarts';
+// 引入柱状图
+import 'echarts/lib/chart/bar';
+// 引入提示框和标题组件
+import 'echarts/lib/component/tooltip';
+import 'echarts/lib/component/title';
+
 import styles from './NewPage.less';
 
 
@@ -51,11 +59,11 @@ class Clock extends React.Component {
                             checkedChildren={'on'}
                             unCheckedChildren={'off'}
                             defaultChecked
-                            onChange={this.onSwitch}>
-                        </Switch>
+                            onChange={this.onSwitch} />
                     </span>
                     <span>
-                        点击开关  {this.state.checked ? '显示' : '隐藏'}.
+                        点击开关  {this.state.checked ? (
+                            <p>显示 {this.state.date.toLocaleTimeString()}.</p>) : 'clock已隐藏'}
                     </span>
                 </h2>
 
@@ -65,12 +73,84 @@ class Clock extends React.Component {
     }
 }
 
+class EchartsTest extends Component {
+    componentDidMount() {
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('main'));
+        // 绘制图表
+        var xAxisData = [];
+        var data1 = [];
+        var data2 = [];
+        for (var i = 0; i < 100; i++) {
+            xAxisData.push('类目' + i);
+            data1.push((Math.sin(i / 5) * (i / 5 - 10) + i / 6) * 5);
+            data2.push((Math.cos(i / 5) * (i / 5 - 10) + i / 6) * 5);
+        }
+
+        myChart.setOption({
+            title: {
+                text: '柱状图动画延迟'
+            },
+            legend: {
+                data: ['bar', 'bar2'],
+                align: 'left'
+            },
+            toolbox: {
+                // y: 'bottom',
+                feature: {
+                    magicType: {
+                        type: ['stack', 'tiled']
+                    },
+                    dataView: {},
+                    saveAsImage: {
+                        pixelRatio: 2
+                    }
+                }
+            },
+            tooltip: {},
+            xAxis: {
+                data: xAxisData,
+                silent: false,
+                splitLine: {
+                    show: false
+                }
+            },
+            yAxis: {
+            },
+            series: [{
+                name: 'bar',
+                type: 'bar',
+                data: data1,
+                animationDelay: function (idx) {
+                    return idx * 10;
+                }
+            }, {
+                name: 'bar2',
+                type: 'bar',
+                data: data2,
+                animationDelay: function (idx) {
+                    return idx * 10 + 100;
+                }
+            }],
+            animationEasing: 'elasticOut',
+            animationDelayUpdate: function (idx) {
+                return idx * 5;
+            }
+        });
+    }
+    render() {
+        return (
+            <div id="main" style={{ width: 800, height: 400 }}></div>
+        );
+    }
+}
 
 class NewPage extends Component {
     render() {
         return (
             <Card>
                 <Clock />
+                <EchartsTest />
             </Card>
         );
     }
